5d1cb378b9914c55115928492b25a55206f03b8a
Douglas Volcato <douglasvolcato@gmail.com>
2022-09-08 15:04:23 -0300

    Added repository routes

---
+++ src/routes/repo.routes.js
@@ -0,0 +1,24 @@
+export class RepoRoutes {
+  constructor(controller, router) {
+    this.controller = controller;
+    this.router = router;
+  }
+
+  route() {
+    this.router.post("/create/:id", (req, res) =>
+      this.controller.createRepoController(req, res)
+    );
+
+    this.router.delete("/delete/:id/:name", (req, res) =>
+      this.controller.createRepoController(req, res)
+    );
+
+    this.router.get("/get-all/:id", (req, res) =>
+      this.controller.getAllRepoController(req, res)
+    );
+
+    this.router.put("/update/:id/:name", (req, res) =>
+      this.controller.updateRepoController(req, res)
+    );
+  }
+}
