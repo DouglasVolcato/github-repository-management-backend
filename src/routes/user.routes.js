a2bb354c791c7cb8c6acc5108e425d8b1cda0190
Douglas Volcato <douglasvolcato@gmail.com>
2022-09-08 15:04:35 -0300

    Added user routes

---
+++ src/routes/user.routes.js
@@ -0,0 +1,34 @@
+export class UserRoutes {
+  constructor(controller, router) {
+    this.controller = controller;
+    this.router = router;
+  }
+
+  route() {
+    this.router.post("/create", (req, res) =>
+      this.controller.createUserController(req, res)
+    );
+
+    this.router.put("/update/:id", (req, res) =>
+      this.controller.updateUserController(req, res)
+    );
+
+    this.router.delete("/delete/:id", (req, res) =>
+      this.controller.deleteUserController(req, res)
+    );
+
+    this.router.get("/get-all", (req, res) =>
+      this.controller.getAllUserController(req, res)
+    );
+
+    this.router.get("/get-by-email/:email", (req, res) =>
+      this.controller.getUserByEmailController(req, res)
+    );
+
+    this.router.get("/get-by-id/:id", (req, res) =>
+      this.controller.getUserByIdController(req, res)
+    );
+
+    return this.router;
+  }
+}
