b10663b97619b8288b592781338ecca9fca80d52
Douglas Volcato <douglasvolcato@gmail.com>
2022-09-07 21:59:24 -0300

    Added repository entity

---
+++ src/entities/repository.entity.js
@@ -0,0 +1,14 @@
+export class Repository {
+  constructor(repository) {
+    this.name = repository.name;
+    this.link = repository.link;
+    this.priority = repository.priority;
+    this.note = repository.note;
+  }
+
+  validate() {
+    if (!this.name || !this.link || !this.priority || !this.note) {
+      throw new Error("Missing required fields for Repository.");
+    }
+  }
+}
