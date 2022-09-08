7394b985dbba8380a7f141eff905b9d59d7d53df
Douglas Volcato <douglasvolcato@gmail.com>
2022-09-08 14:37:57 -0300

    Fixed updateRepo export

--- src/services/repo.services.index.js
+++ src/services/repo.services.index.js
@@ -1,4 +1,4 @@
 export { CreateRepoUseCase } from "./repo/repo.create.js";
 export { DeleteRepoUseCase } from "./repo/repo.delete.js";
 export { GetAllRepoUseCase } from "./repo/repo.getAll.js";
-export { UpdateReposUseCase } from "./repo/repo.update.js";
+export { UpdateRepoUseCase } from "./repo/repo.update.js";
