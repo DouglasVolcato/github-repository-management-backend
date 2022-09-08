ecd67a050bbc226259b5cb578a1a415b7af1446c
Douglas Volcato <douglasvolcato@gmail.com>
2022-09-07 21:55:39 -0300

    Added user entity

---
+++ src/entities/user.entity.js
@@ -0,0 +1,27 @@
+import { randomUUID } from "node:crypto";
+
+export class User {
+  constructor(user) {
+    this.id = randomUUID();
+    this.name = user.name;
+    this.email = user.email;
+    this.photo = user.photo;
+    this.repositories = [...user.repositories];
+  }
+
+  validate() {
+    if (this.id || this.name || this.email) {
+      throw new Error("Missing required fields for user.");
+    }
+  }
+
+  getUser() {
+    return {
+      id: this.id,
+      name: this.name,
+      email: this.email,
+      photo: this.photo,
+      repositories: this.repositories,
+    };
+  }
+}
