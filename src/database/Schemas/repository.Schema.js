1319a78b2a7424317a89303399d9a4bb202e8d13
Douglas Volcato <douglasvolcato@gmail.com>
2022-09-07 21:48:48 -0300

    removed id

--- src/database/Schemas/repository.Schema.js
+++ src/database/Schemas/repository.Schema.js
@@ -1,7 +1,6 @@
 import { Schema } from "mongoose";
 
 export const repositorySchema = new Schema({
-  id: { type: String, required: true },
   name: { type: String, required: true },
   link: { type: String, required: true },
   priority: { type: String, required: true },
