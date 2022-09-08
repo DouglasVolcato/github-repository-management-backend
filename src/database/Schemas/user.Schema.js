dc29a04942f21066c09f2e74b1af8e037a09fe1c
Douglas Volcato <douglasvolcato@gmail.com>
2022-09-07 21:40:10 -0300

    Added User schema

---
+++ src/database/Schemas/user.Schema.js
@@ -0,0 +1,12 @@
+import { Schema, model } from "mongoose";
+import repositorySchema from "./repository.Schema";
+
+const userSchema = new Schema({
+  id: { type: String, required: true },
+  name: { type: String, required: true },
+  email: { type: String, reqired: true, select: false },
+  photo: { type: String },
+  repositories: { repositorySchema },
+});
+
+export const userModel = model("User", userSchema);
