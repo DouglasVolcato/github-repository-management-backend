7e837af3e91633b5708b4247b4a64e0ddc6cc11c
Douglas Volcato <douglasvolcato@gmail.com>
2022-09-09 20:55:20 -0300

    Added auth.middlewares

---
+++ src/middlewares/auth.middlewares.js
@@ -0,0 +1,41 @@
+import jwt from "jsonwebtoken";
+
+export class AuthMiddlewares {
+  constructor(services) {
+    this.services = services;
+  }
+
+  verifyToken(req, res, next) {
+    try {
+      const authorization = req.headers.authorization;
+
+      if (!authorization) {
+        throw new Error("The token was not informed.");
+      }
+
+      const split = authorization.split(" ");
+
+      if (!split || split[0] !== "Bearer" || split.length !== 2) {
+        throw new Error("Invalid token.");
+      }
+
+      jwt.verify(split[1], process.env.SECRET, async (err, decoded) => {
+        if (err) {
+          throw new Error("Invalid token.");
+        }
+
+        const user = await this.services.getUserByIdUseCase.execute(decoded.id);
+
+        if (!user || !user.id) {
+          throw new Error("Invalid token.");
+        }
+
+        req.userId = user.id;
+
+        return next();
+      });
+    } catch (err) {
+      return res.status(401).send({ message: "Authentication error. " + err });
+    }
+  }
+}
