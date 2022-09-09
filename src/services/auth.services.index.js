ee0de94e244fe11b60ffea35357bdc0d46ee641c
Douglas Volcato <douglasvolcato@gmail.com>
2022-09-09 19:45:22 -0300

    Added auth services index

---
+++ src/services/auth.services.index.js
@@ -0,0 +1,3 @@
+export { AuthGetByEmail } from "./auth/auth.getByEmail.js";
+export { AuthVerifyPassword } from "./auth/auth.verifyPassword.js";
+export { AuthGenerateToken } from "./auth/auth.generateToken.js";
