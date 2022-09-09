3ff7ece0a07b31c2f24d6b160ba324c66202b916
Douglas Volcato <douglasvolcato@gmail.com>
2022-09-09 00:26:48 -0300

    Added main index

---
+++ index.js
@@ -0,0 +1,24 @@
+import express, { Router } from "express";
+import cors from "cors";
+import { config } from "dotenv";
+import { Port } from "./src/ports/port.js";
+import { makeUserFactory } from "./src/factories/user.factory.js";
+import { makeRepoFactory } from "./src/factories/repo.factory.js";
+import { MongoDbConnection } from "./src/database/connection/connect.js";
+
+config();
+MongoDbConnection.connectDb();
+
+const app = express();
+const router = Router();
+
+const repoFactory = makeRepoFactory(router);
+const userFactory = makeUserFactory(router);
+
+app.use(express.json());
+app.use(cors());
+
+app.use("/repo", repoFactory);
+app.use("/user", userFactory);
+
+Port.portConnect(app);
