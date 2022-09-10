cbeed64182ae6a9fe301e9af9312ff58e3021df5
Douglas Volcato <douglasvolcato@gmail.com>
2022-09-09 23:55:07 -0300

    Fixed syncronous method

--- src/services/auth/auth.generateToken.js
+++ src/services/auth/auth.generateToken.js
@@ -1,7 +1,7 @@
 import jwt from "jsonwebtoken";
 
 export class GenerateTokenUseCase {
-  async execute(userId) {
+  execute(userId) {
     return jwt.sign({ id: userId }, process.env.SECRET, {
       expiresIn: 86400,
     });
