774eac3d68e06c048a2618f13ed287ea81756e1c
Douglas Volcato <douglasvolcato@gmail.com>
2022-09-09 23:54:45 -0300

    Added midleware

--- src/routes/repo.routes.js
+++ src/routes/repo.routes.js
@@ -1,24 +1,33 @@
 export class RepoRoutes {
-  constructor(controller, router) {
+  constructor(controller, middleware, router) {
     this.controller = controller;
+    this.middleware = middleware;
     this.router = router;
   }
 
   route() {
-    this.router.post("/create/:id", (req, res) =>
-      this.controller.createRepoController(req, res)
+    this.router.post(
+      "/create/:id",
+      (req, res, next) => this.middleware.verifyToken(req, res, next),
+      (req, res) => this.controller.createRepoController(req, res)
     );
 
-    this.router.delete("/delete/:id/:name", (req, res) =>
-      this.controller.deleteRepoController(req, res)
+    this.router.delete(
+      "/delete/:id/:name",
+      (req, res, next) => this.middleware.verifyToken(req, res, next),
+      (req, res) => this.controller.deleteRepoController(req, res)
     );
 
-    this.router.get("/get-all/:id", (req, res) =>
-      this.controller.getAllRepoController(req, res)
+    this.router.get(
+      "/get-all/:id",
+      (req, res, next) => this.middleware.verifyToken(req, res, next),
+      (req, res) => this.controller.getAllRepoController(req, res)
     );
 
-    this.router.put("/update/:id/:name", (req, res) =>
-      this.controller.updateRepoController(req, res)
+    this.router.put(
+      "/update/:id/:name",
+      (req, res, next) => this.middleware.verifyToken(req, res, next),
+      (req, res) => this.controller.updateRepoController(req, res)
     );
 
     return this.router;
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,6 +1,7 @@
 export class UserRoutes {
-  constructor(controller, router) {
+  constructor(controller, middleware, router) {
     this.controller = controller;
+    this.middleware = middleware;
     this.router = router;
   }
 
@@ -9,24 +10,34 @@ export class UserRoutes {
       this.controller.createUserController(req, res)
     );
 
-    this.router.put("/update/:id", (req, res) =>
-      this.controller.updateUserController(req, res)
+    this.router.put(
+      "/update/:id",
+      (req, res, next) => this.middleware.verifyToken(req, res, next),
+      (req, res) => this.controller.updateUserController(req, res)
     );
 
-    this.router.delete("/delete/:id", (req, res) =>
-      this.controller.deleteUserController(req, res)
+    this.router.delete(
+      "/delete/:id",
+      (req, res, next) => this.middleware.verifyToken(req, res, next),
+      (req, res) => this.controller.deleteUserController(req, res)
     );
 
-    this.router.get("/get-all", (req, res) =>
-      this.controller.getAllUserController(req, res)
+    this.router.get(
+      "/get-all",
+      (req, res, next) => this.middleware.verifyToken(req, res, next),
+      (req, res) => this.controller.getAllUserController(req, res)
     );
 
-    this.router.get("/get-by-email/", (req, res) =>
-      this.controller.getUserByEmailController(req, res)
+    this.router.get(
+      "/get-by-email/",
+      (req, res, next) => this.middleware.verifyToken(req, res, next),
+      (req, res) => this.controller.getUserByEmailController(req, res)
     );
 
-    this.router.get("/get-by-id/:id", (req, res) =>
-      this.controller.getUserByIdController(req, res)
+    this.router.get(
+      "/get-by-id/:id",
+      (req, res, next) => this.middleware.verifyToken(req, res, next),
+      (req, res) => this.controller.getUserByIdController(req, res)
     );
 
     return this.router;
