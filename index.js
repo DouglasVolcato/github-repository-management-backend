e257b7b58c1b716c3c74b5ce08eb741240166d05
Douglas Volcato <douglasvolcato@gmail.com>
2022-09-09 21:38:59 -0300

    Added auth route

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ import { config } from "dotenv";
 import { Port } from "./src/ports/port.js";
 import { makeUserFactory } from "./src/factories/user.factory.js";
 import { makeRepoFactory } from "./src/factories/repo.factory.js";
+import { makeAuthFactory } from "./src/factories/auth.factory.js";
 import { MongoDbConnection } from "./src/database/connection/connect.js";
 
 config();
@@ -14,11 +15,13 @@ const router = Router();
 
 const repoFactory = makeRepoFactory(router);
 const userFactory = makeUserFactory(router);
+const authFactory = makeAuthFactory(router);
 
 app.use(express.json());
 app.use(cors());
 
 app.use("/repo", repoFactory.route());
 app.use("/user", userFactory.route());
+app.use("/auth", authFactory.route());
 
 Port.portConnect(app);
