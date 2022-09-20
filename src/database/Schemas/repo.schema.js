44365e10f915b10c95f62a3545039819588564b6
Douglas Volcato <douglasvolcato@gmail.com>
2022-09-20 01:26:08 -0300

    Added deaddile atribute for repositories

--- src/database/Schemas/repo.schema.js
+++ src/database/Schemas/repo.schema.js
@@ -4,6 +4,7 @@ const repoSchema = new mongoose.Schema({
   name: { type: String, required: true },
   link: { type: String, required: true },
   priority: { type: String, required: true },
+  deadline: { type: String, required: true },
   note: { type: String, required: true },
 });
 
--- src/entities/repo.entity.js
+++ src/entities/repo.entity.js
@@ -3,11 +3,18 @@ export default class Repository {
     this.name = repo.name;
     this.link = repo.link;
     this.priority = repo.priority;
+    this.deadline = repo.deadline;
     this.note = repo.note;
   }
 
   validate() {
-    if (!this.name || !this.link || !this.priority || !this.note) {
+    if (
+      !this.name ||
+      !this.link ||
+      !this.priority ||
+      !this.deadline ||
+      !this.note
+    ) {
       throw new Error("Missing required fields for Repository.");
     }
   }
@@ -17,6 +24,7 @@ export default class Repository {
       name: this.name,
       link: this.link,
       priority: this.priority,
+      deadline: this.deadline,
       note: this.note,
     };
   }
